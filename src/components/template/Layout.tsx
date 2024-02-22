5dfa812907adfbaf3642f5edcb66cd211babc701
abdellah-ch <abdellahchotta55@gmail.com>
2024-02-23 00:07:59 +0100

    pricing component and footer

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../organisms/NavBar";
+import Footer from "../organisms/Footer";
 const Layout = () => {
 
     return (
         <main >
             <NavBar />
             <Outlet />
+            <Footer />
         </main>
     )
 }
